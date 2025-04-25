ca8063333fcff11dc6f864c6e6cfe989985b8438
Otmane157 <otmanerpcba@gmail.com>
2025-04-25 10:27:38 +0100

    feat(frontend): add website footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -10,50 +10,58 @@ function TopFooter() {
             alt="Logo Ofppt"
             className="w-[327px] h-[102px]  ms-[200px]"
           />
-          <div className="w-[327px] ms-[200px] mt-2  border-b-[1px] border-dashed" />
-          <div className="flex justify-start gap-9 ms-[220px]">
+          <div className="w-[327px] ms-[200px] mt-2  border-b-[1px] border-dashed border-white" />
+          <div className="flex justify-start gap-9 ms-[220px] ">
+            <a href="#">
             <img
               src="https://img.icons8.com/?size=100&id=8818&format=png&color=FFFFFF"
               className="size-10 mt-5"
               alt="SocialMediaIcone"
             />
+            </a>
+            <a href="#">
             <img
               src="https://img.icons8.com/?size=100&id=59813&format=png&color=FFFFFF"
               className="size-10 mt-5"
               alt="SocialMediaIcone"
             />
+            </a>
+            <a href="#">
             <img
               src="https://img.icons8.com/?size=100&id=phOKFKYpe00C&format=png&color=FFFFFF"
               className="size-10 mt-5"
               alt="SocialMediaIcone"
             />
+            </a>
+            <a href="#">
             <img
               src="https://img.icons8.com/?size=100&id=IuI5Yd3J3qcC&format=png&color=FFFFFF"
               className="size-10  mt-5 "
               alt="SocialMediaIcone"
             />
+            </a>
           </div>
         </div>
-        <div className="border-s-[0.1px] border-dashed mt-16 h-[370px]" />
+        <div className="border-s-[0.1px] border-dashed mt-16 h-[370px] border-white" />
         <div className="flex flex-col gap-7   p-[80px]">
           <div>
             <div className="font text-white text-xl font-bold ">
               Lient utiles
             </div>
-            <div className="border-b-4 w-[48px] mt-[7px]" />
+            <div className="border-b-4 border-white w-[48px] mt-[7px]" />
           </div>
           <ul className="flex flex-col gap-5 text-white font">
-            <li>Ofppt Site Officiel</li>
-            <li>MY WAY</li>
-            <li>Altissia/OFPPT-Langues</li>
-            <li>Scholar VOX</li>
+            <li><a href="#">Ofppt Site Officiel</a> </li>
+            <li><a href="#">MY WAY</a> </li>
+            <li><a href="#">Altissia/OFPPT-Langues</a> </li>
+            <li><a href="#">Scholar VOX</a> </li>
           </ul>
         </div>
-        <div className="border-s-[0.1px] border-dashed mt-16 h-[370px]" />
+        <div className="border-s-[0.1px] border-dashed mt-16 h-[370px] border-white" />
         <div className="flex flex-col gap-5  p-[80px]">
           <div>
             <div className="font text-white text-xl font-bold ">Contacts</div>
-            <div className="border-b-4 w-[48px] mt-[7px]" />
+            <div className="border-b-4 border-white w-[48px] mt-[7px]" />
           </div>
           <ul className=" list-disc text-white font  space-y-2 ms-8">
             <li>Adresse</li>
